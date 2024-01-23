a15b52abde4b3840ddb3e6a213f837371b0ba1b9
kysoosi <samidemus@gmail.com>
2024-01-23 15:26:57 +0200

    first commit

---
+++ Plussaa tai Miinusta/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//Sami Kytö INTIM23A6
+
+Console.WriteLine("Anna luku 1");
+var numero1 = Console.ReadLine(); 
+var luku1 = float.Parse(numero1);
+Console.WriteLine("Anna luku 2");
+var numero2 = Console.ReadLine();
+var luku2 = float.Parse(numero2);
+//tein ensin vain var luku1 = float.Parse(Console.ReadLine());
+//ilman erillistä float.Parse()
+//mutta muutin tällaiseksi koska vihreä aaltoviiva varoitus
+//antoi silloin enemmän tekstiä kuin näin -Sami
+//tässä esimerkki: var luku3 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Tulos on: " + Math.Round(plusTaiMinus(luku1, luku2), 2));
+static float plusTaiMinus(float a, float b)
+{
+    if (a < b)
+    {
+        float tulos = a - b;
+        return tulos;
+        
+    }
+    else
+    {
+        float tulos = a + b;
+        return tulos;
+    }
+}
